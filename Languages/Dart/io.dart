32c2dae83db27bfe5c956138cfee0ff34b2aa0c2
Janhavi404 <68691067+Janhavi404@users.noreply.github.com>
2020-10-02 12:09:08 +0530

    Create io.dart

---
+++ Languages/Dart/io.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  print("Enter your name?");
+  String name = stdin.readLineSync();
+  print("Hello, $name! \nWelcome to GeeksforGeeks!!");
+}
+
+// contributed by Janhavi Jain (github: Janhavi404)
